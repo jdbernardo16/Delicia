6e5c153eebb3af93ca841a423e211fc49c176033
jdbernardo16 <jdbernardo16@gmail.com>
2019-01-17 15:11:33 +0800

    With footer

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,10 @@
-    $(document).ready(function(){
-     $('.home-frame2__slider-container').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
+$(document).ready(function(){
+	$('.home-frame2__slider-container').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	dots: false
+	});
 });
-    });
-
+   
